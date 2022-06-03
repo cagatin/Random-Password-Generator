24c9610d417699ee7d6a5f4769f1b5f94e1019bb
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-03 12:06:26 -0700

    Fixed funcitonality of generatePassword so charString begins as an empty string

--- script.js
+++ script.js
@@ -15,22 +15,47 @@ generateBtn.addEventListener("click", writePassword);
 // Function used to generate a password.
 function generatePassword() {
   // string variable containing all possible characters to choose from.
-  let charString = "~!@#$%^&*()-=_+[]{}:;,./?|0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let charString = "";
+
+  let specialChars = "~!@#$%^&*()-=_+[]{}:;,./?|";
+  let numberChars = "0123456789";
+  let capitalChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 
   // prompt the user for the desired length of the password.
   let passLength = prompt("Please enter the desired length of your password. (Between 8 and 128)");
 
-  // prompt the user to allow lowercase characters
+  // prompt the user to allow special character values.
   let allowSpecial = confirm("Allow password to contain special character values?");
 
-  // prompt the user to allow numbers
+  // prompt the user to allow number values.
   let allowNumbers = confirm("Allow password to contain number values?");
 
-  // if the allowSpecial variable contains a false boolean value, then remove the special characters from the charString. 
-  if (!allowSpecial) {
-    charString = charString.substring(27, charString.length);
-  } else if (!allowNumbers) {
-    charString = charString.substring(10, charString.length);
+  // prompt to allow capital letters.
+  let allowCapitals = confirm("Allow password to contain capital letters?");
+
+  // prompt to allow lowercase eltters
+  let allowLowercase = confirm("allow password to contian lowercase letters?");
+
+  // If the allowSpecial is true, add specialChars to the charString.
+  if (allowSpecial) {
+    charString = charString.concat(specialChars);
+  }
+  // If allowNumbers is true, add the numberChars to the charString.
+  if (allowNumbers) {
+    charString = charString.concat(numberChars);
+  }
+  // If allowCapitals is true, add the capitalChars to the charString.
+  if (allowCapitals) {
+    charString = charString.concat(capitalChars);
+  }
+  if (allowLowercase) {
+    charString = charString.concat(lowercaseChars);
+  }
+
+  if (charString == "") {
+    alert("No character types selected. Please Select at least one character type to generate your password.");
+    return;
   }
 
   // password to be returned by the function.
