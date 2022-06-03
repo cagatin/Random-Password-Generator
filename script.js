17cd95397ea6103b2cde2e377301fe89004ce2e8
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-03 10:12:14 -0700

    created reference for text area and began work on generatePassword function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Assignment Code
+// Getting References to the elements we need.
 var generateBtn = document.querySelector("#generate");
+var textArea = document.querySelector('#textArea');
+
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +10,16 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //appending the generated password to the textArea.
+  textArea.appendChild(passwordText);
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Function used to generate a password.
+function generatePassword() {
+  // prompt the user for the desired length of the password.
+  let passLength = prompt("Please enter the desired length of your password. (Between 8 and 128)");
+}
