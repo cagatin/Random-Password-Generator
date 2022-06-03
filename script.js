6ceb82f135006572da806e58d7ab8b03edc06da8
Felix Cagatin <cagatingilbert@gmail.com>
2022-06-03 10:33:38 -0700

    Added functionality for generating passwords without special characters

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // Getting References to the elements we need.
 var generateBtn = document.querySelector("#generate");
-var textArea = document.querySelector('#textArea');
-
 
 // Write password to the #password input
 function writePassword() {
@@ -9,14 +7,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  //appending the generated password to the textArea.
-  textArea.appendChild(passwordText);
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // Function used to generate a password.
 function generatePassword() {
@@ -24,5 +18,17 @@ function generatePassword() {
   let passLength = prompt("Please enter the desired length of your password. (Between 8 and 128)");
 
   // string variable containing all possible characters to choose from.
-  let charString = "0123456789~!@#$%^&*()-=_+[]{}:;,./?|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let charString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let specialString = "~!@#$%^&*()-=_+[]{}:;,./?|";
+
+  // password to be returned by the function.
+  let result = "";
+
+  // for loop used to generate random password. 
+  for (let i = 0; i < passLength; i++) {
+    let charIndex = Math.floor(Math.random() * charString.length);
+    result += charString.charAt(charIndex);
+  }
+  console.log(result);
+  return result;
 }
